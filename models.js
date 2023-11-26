851b792ea22ea0f879febeca6c6a5fa30727ead2
sparrow2100 <111162787+sparrow2100@users.noreply.github.com>
2023-11-26 14:29:28 -0500

    Update project to work with database

---
+++ models.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+let eraSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+let composerSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  life: {
+    fullName: { type: String, required: true },
+    lifespan: { type: String, required: true },
+    bio: { type: String, required: true },
+    nationality: { type: String, required: true },
+  },
+  era: { type: mongoose.Schema.Types.ObjectId, ref: "Era" },
+  img: { type: String, required: true },
+  imgCredit: String,
+  works: [{ piece: String, date: String, description: String }],
+});
+
+let userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  birthday: Date,
+  favouriteComposers: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Composer" },
+  ],
+});
+
+let Composer = mongoose.model("Composer", composerSchema);
+let Era = mongoose.model("Era", eraSchema);
+let User = mongoose.model("User", userSchema);
+
+module.exports.Composer = Composer;
+module.exports.Era = Era;
+module.exports.User = User;
