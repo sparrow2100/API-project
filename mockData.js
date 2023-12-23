3603c0739836bea752c2f91af3773547f57c6b3c
sparrow2100 <111162787+sparrow2100@users.noreply.github.com>
2023-12-23 11:26:28 -0500

    Remove commented code from index.js

---
+++ mockData.js
@@ -0,0 +1,345 @@
+//composers array
+
+// let topComposers = [
+//   {
+//     name: "Elizabeth Maconchy",
+//     life: {
+//       fullName: "Elizabeth Maconchy",
+//       lifespan: "1907-1994",
+//       bio: "Elizabeth Maconchy was an Irish/English composer born in 1907 in Broxbourne, England. Her family soon moved to Ireland, where she studied music in Dublin. At age sixteen, she studied at the Royal College of Music in London under Ralph Vaughan Williams and Charles Wood. After the RCM, she continued her musical studies in Prague. Besides composing, Maconchy worked throughout her life to improve the lives of composers. She organised concerts in London in response to a lack of opportunities for female composers, and was also the Chair of the Composers Guild of Great Britain for many years (the first woman to hold that position), and President of the Society for the Promotion of New Music. She is considered to be one of the greatest composers of the British Isles. ",
+//       nationality: "Irish/English",
+//     },
+//     era: {
+//       name: "modern",
+//       description:
+//         "The modern era in classical music, roughly from the late 19th to the mid-20th century, witnessed a departure from traditional tonal structures and the exploration of dissonance. Composers during this period experimented with new forms, tonalities, and embraced diverse musical styles, reflecting the societal and technological shifts of the time.",
+//     },
+
+//     img: "https://en.wikipedia.org/wiki/Elizabeth_Maconchy#/media/File:Elizabeth_Maconchy.png",
+//     works: [
+//       {
+//         piece: "string quartet no.3",
+//         date: "1938",
+//         description:
+//           "The third of thirteen string quartets written by Maconchy.",
+//       },
+//       {
+//         piece: "Serenata Concertante for violin and orchestra",
+//         date: "1962",
+//         description: "A piece for solo violin and orchestra.",
+//       },
+//       {
+//         piece: "My Dark Heart",
+//         date: "1981",
+//         description:
+//           "Song cycle for soprano and chamber ensemble, with text by J.M. Synge.",
+//       },
+//     ],
+//   },
+//   {
+//     name: "Lili Boulanger",
+//     life: {
+//       fullName: "Lili Boulanger",
+//       lifespan: "1893-1918",
+//       bio: "The multitalented Lili Boulanger was a French composer and musician born in 1893 in Paris. She studied at the Paris Conservatoire from a young age, learning voice, piano, violin, cello, and harp, in addition to harmony, counterpoint and composition under Director Gabriel Fauré. At age nineteen she was the first woman to win the coveted Prix de Rome with her composition Faust et Hélène, after collapsing from illness during the performance of her previous year’s attempt. She died from intestinal tuberculosis at the young age of 24, after a short but prolific life.",
+//       nationality: "French",
+//     },
+
+//     era: {
+//       name: "romantic",
+//       description:
+//         "The romantic era in classical music, spanning roughly the 19th century, emphasized individual expression and emotion, embraced rich harmonies, and championed programmatic storytelling through their music. Characterized by the pursuit of intense personal expression and the exploration of diverse themes, this era witnessed the rise of nationalism, virtuosic performances, and the birth of iconic masterpieces.",
+//     },
+
+//     img: "https://upload.wikimedia.org/wikipedia/commons/d/d6/Lili_Boulanger_par_Henri_Manuel.jpg",
+//     works: [
+//       {
+//         piece: "Les Sirènes",
+//         date: "1911",
+//         description: "For solo soprano,three part choir, and piano.",
+//       },
+//       {
+//         piece: "Faust et Hélène",
+//         date: "1913",
+//         description:
+//           "Prix de Rome winning cantata for mezzo-soprano, tenor, baritone, chorus and orchestra.",
+//       },
+//       {
+//         piece: "D'un Soir Triste",
+//         date: "1917-1918",
+//         description: "Symphonic poem for orchestra.",
+//       },
+//     ],
+//   },
+//   {
+//     name: "Fanny Mendelssohn",
+//     life: {
+//       fullName: "Fanny Mendelssohn",
+//       lifespan: "1805-1847",
+//       bio: "Fanny Mendelssohn (Fanny Hensel after marriage) was an early romantic German composer and pianist. She was overshadowed both in life and in history by her famous brother Felix Mendelssohn, and published many works under his name. She and Felix both studied with the same piano instructor, who gave her the ‘high praise’ “she plays like a man”, and composition instructor, who favoured Fanny’s composition skills over Felix’s. Both her father and brother seem to have been tolerant but disapproving of her composition, as they didn’t think it was a fitting activity for a woman. However, Felix often wrote to his sister to ask for advice on his compositions, and later Fanny’s husband was supportive of her publishing her works. In her life Fanny composed over 450 works, mostly piano pieces and lieder. ",
+//       nationality: "German",
+//     },
+
+//     era: {
+//       name: "romantic",
+//       description:
+//         "The romantic era in classical music, spanning roughly the 19th century, emphasized individual expression and emotion, embraced rich harmonies, and championed programmatic storytelling through their music. Characterized by the pursuit of intense personal expression and the exploration of diverse themes, this era witnessed the rise of nationalism, virtuosic performances, and the birth of iconic masterpieces.",
+//     },
+
+//     img: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Fanny_Hensel_1842.jpg",
+//     works: [
+//       {
+//         piece:
+//           "Sechs Lieder für eine Simme mit Begleitung des Pianoforte, Op.7",
+//         date: "1847",
+//         description: "Songs without words for the piano.",
+//       },
+//       {
+//         piece: "Piano trio in D minor, Op. 11",
+//         date: "1850",
+//         description: "Published posthumously.",
+//       },
+//       {
+//         piece: "Das Jahr: 12 Characterstucke No.12. December",
+//         date: "1841",
+//         description:
+//           "One of 12 piano pieces depicting the 12 months of the year.",
+//       },
+//     ],
+//   },
+//   {
+//     name: "Sophie-Carmen Eckhardt-Gramatte",
+//     life: {
+//       fullName: "Sophie-Carmen Eckhardt-Gramatte",
+//       lifespan: "1899-1974",
+//       bio: "Eckhardt-Gramatte was a Russian-Canadian virtuoso violinist and composer. She was born in 1899 in Moscow, and grew up first in England then in France where she studied at the Paris Conservatoire. She lived in Barcelona and later Vienna following her two marriages, where she composed and played concert tours. In 1953 she and her second husband moved to Winnipeg in Manitoba, Canada, where she continued to compose and became a well-known violin and piano pedagogue, passing down her ideas to notable Canadian musicians/music teachers such as violinist Gwen Thompson and pianist Diedre Irons. Some of her most notable compositions are her ten caprices for solo violin, which are known for their extreme technical difficulty. ",
+//       nationality: "Russian/Canadian",
+//     },
+
+//     era: {
+//       name: "modern",
+//       description:
+//         "The modern era in classical music, roughly from the late 19th to the mid-20th century, witnessed a departure from traditional tonal structures and the exploration of dissonance. Composers during this period experimented with new forms, tonalities, and embraced diverse musical styles, reflecting the societal and technological shifts of the time.",
+//     },
+
+//     img: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Sophie_Gramatt%C3%A9.jpg",
+//     works: [
+//       {
+//         piece: "Symphony No.1 in C major, E.117",
+//         date: "1946",
+//         description: "Eckhardt-Gramatte's first symphony.",
+//       },
+//       {
+//         piece: "Le Départ D'un Train",
+//         date: "1927",
+//         description: "Caprice No.7 for solo violin.",
+//       },
+//       {
+//         piece: "Duo for viola and cello",
+//         date: "1944",
+//         description: "E.109",
+//       },
+//     ],
+//   },
+//   {
+//     name: "Clara Schumann",
+//     life: {
+//       fullName: "Clara Schumann",
+//       lifespan: "1819-1896",
+//       bio: "Clara Schumann was a German concert pianist and composer of the romantic era. She was born in Leipzig, to parents who were both pianists and piano teachers. She lived with her father following her parent’s divorce, and he trained her as a pianist. She was considered a prodigy,  beginning to tour Europe at the young age of eleven after her debut at age nine. Unfortunately, today she is perhaps most well known for her part in a ‘love triangle’ between herself, and famous composers Robert Schumann and Johannes Brahms (Robert Schumann later becoming her husband). However, she was so admired at the time that among other honours she received a medal from Goethe, an offer from Niccolo Paganini to appear with her during a tour in Paris, enthusiastic praise from Franz Liszt, and a poem in her praise by Franz Grillparzer titled 'Clara Wieck and Beethoven'. After Robert’s death she continued to tour Europe for decades, and was appointed a teaching position at Dr. Hoch’s Konservatorium in Frankfurt, the only woman on the faculty. Despite her immense talent and the respect she commanded in the musical community, as she grew older and her other responsibilities grew, she became discouraged and is quoted as saying 'I once believed that I possessed a creative talent, but I have given up this idea; a woman must not desire to compose– there has never yet been one able to do it. Should I expect to be the one?'",
+//       nationality: "German",
+//     },
+
+//     era: {
+//       name: "romantic",
+//       description:
+//         "The romantic era in classical music, spanning roughly the 19th century, emphasized individual expression and emotion, embraced rich harmonies, and championed programmatic storytelling through their music. Characterized by the pursuit of intense personal expression and the exploration of diverse themes, this era witnessed the rise of nationalism, virtuosic performances, and the birth of iconic masterpieces.",
+//     },
+
+//     img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Andreas_Staub_-_Clara_Wieck_%28Lithographie_1839%29.jpg/800px-Andreas_Staub_-_Clara_Wieck_%28Lithographie_1839%29.jpg",
+//     works: [
+//       {
+//         piece: "Piano Concerto in A minor",
+//         date: "1836",
+//         description: "Op.7, for solo piano and orchestra.",
+//       },
+//       {
+//         piece: "Piano Sonata in G minor",
+//         date: "1842",
+//         description:
+//           "Sonata for solo piano. Written 1841-1842 and published in 1991.",
+//       },
+//       {
+//         piece: "Valses romantiques",
+//         date: "1835",
+//         description: "For piano.",
+//       },
+//     ],
+//   },
+//   {
+//     name: "Barbara Strozzi",
+//     life: {
+//       fullName: "Barbara Strozzi",
+//       lifespan: "1619-1677",
+//       bio: "Barbara Strozzi was born in Venice in 1619. Her mother is unknown, and she is assumed to be the illegitimate child of poet/librettist Giulio Strozzi, who referred to her as his adopted daughter. She was a virtuoso vocalist, and could also play the lute and the theorbo in addition to studying composition with Francesco Cavalli. She is known for her large body of secular compositions, as well as her poetry and lyrics. ",
+//       nationality: "Italian",
+//     },
+
+//     era: {
+//       name: "baroque",
+//       description:
+//         "The baroque era, from the early 17th to the mid-18th century, was marked by ornate and expressive music, elaborate ornamentation, and the emergence of instrumental forms like the concerto and the sonata. ",
+//     },
+
+//     img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Bernardo_Strozzi_001.jpg/800px-Bernardo_Strozzi_001.jpg",
+//     works: [
+//       {
+//         piece: "Lagrime mie",
+//         date: "1659",
+//         description: "Cantata (lament).",
+//       },
+//       {
+//         piece: "Se volete cosi me ne contento.",
+//         date: "possibly 1657",
+//         description: "For soprano and basso continuo.",
+//       },
+//     ],
+//   },
+//   {
+//     name: "Francesca Caccini",
+//     life: {
+//       fullName: "Francesca Caccini",
+//       lifespan: "1587-1640",
+//       bio: "Francesca Caccini was an Italian composer, musician, and music teacher. She started her career performing in an ensemble with her family, including at least her parents, half-brother, and sister. After the ensemble broke up, Francesca was a court musician at the Medici court, working as a singer, composer, and teacher, including teaching a Medici princess. Most of her music no longer survives, although her opera ‘La liberazione di Ruggiero’ is thought by some to be the oldest opera by a female composer.",
+//       nationality: "Italian",
+//     },
+
+//     era: {
+//       name: "baroque",
+//       description:
+//         "The baroque era, from the early 17th to the mid-18th century, was marked by ornate and expressive music, elaborate ornamentation, and the emergence of instrumental forms like the concerto and the sonata. ",
+//     },
+
+//     img: "https://upload.wikimedia.org/wikipedia/commons/a/ab/Francesca_Caccini.jpg",
+//     works: [
+//       {
+//         piece: "Chi Desia di Saper",
+//         date: "1618",
+//         description: "For voice and continuo.",
+//       },
+//       {
+//         piece: "Rinaldo Inamorato",
+//         date: "1626",
+//         description: "Commissioned by Prince Wladislaw of Poland.",
+//       },
+//       {
+//         piece: "La Liberazione di Ruggiero dall'isola d'Alcina",
+//         date: "1625",
+//         description: "Musical comedy",
+//       },
+//     ],
+//   },
+//   {
+//     name: "Elisabeth Jacquet de La Guerre",
+//     life: {
+//       fullName: "Elisabeth Jacquet de La Guerre",
+//       lifespan: "1665-1729",
+//       bio: "Elisabeth Jacquet de La Guerre was a French composer and harpsichordist born into a family of composers, musicians and instrument makers. Her father educated her in music from a young age, and at the age of five she performed at Versailles for Louis XIV. She joined and was educated in the French court, and continued her musical career even after marriage– teaching, composing, and playing in concerts. Her only opera wasn’t well received, due to her use of more contemporary, innovative techniques that were not yet popular in France, but she was renowned for her sonatas. ",
+//       nationality: "French",
+//     },
+
+//     era: {
+//       name: "baroque",
+//       description:
+//         "The baroque era, from the early 17th to the mid-18th century, was marked by ornate and expressive music, elaborate ornamentation, and the emergence of instrumental forms like the concerto and the sonata. ",
+//     },
+
+//     img: "https://upload.wikimedia.org/wikipedia/commons/b/ba/Elisabeth_Jacquet_de_La_Guerre-full.jpg",
+//     works: [
+//       {
+//         piece: "Suite in A minor",
+//         date: "1687",
+//         description: "from les pièces de clavessin",
+//       },
+//       {
+//         piece: "Sonata in D minor",
+//         date: "1695",
+//         description: "For violin, viola da gamba and basso continuo.",
+//       },
+//       {
+//         piece: "Le Sommeil d'Ulysse",
+//         date: "1715",
+//         description: "Cantata for soprano, flute, strings and harpsichord.",
+//       },
+//     ],
+//   },
+//   {
+//     name: "Hildegard von Bingen",
+//     life: {
+//       fullName: "Hildegard von Bingen",
+//       lifespan: "1098-1179",
+//       bio: "Hildegard von Bingen was a German abbess in the 11th and 12th centuries known not only for her sacred music, but for being a writer, philosopher, mystic, and the founder of scientific natural history in Germany and two monasteries. She wrote both the music and the lyrics of her compositions, and more of her chants survive than those of any other composer of the middle ages. Hildegard experienced religious visions starting from a young age, gained followers, and has been listed as a Saint. Seventy of her compositions, including a morality play called ‘Ordo Virtutum’, currently survive.",
+//       nationality: "German",
+//     },
+
+//     era: {
+//       name: "medieval",
+//       description:
+//         "The medieval era in classical music, spanning approximately the 5th to the 15th century, was characterized by a sacred and monophonic musical landscape, where Gregorian chant and plainchant flourished. This era embraced modal scales, intricate vocal textures, and often centred around Christian themes.",
+//     },
+
+//     img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Hildegard_von_Bingen.jpg/800px-Hildegard_von_Bingen.jpg",
+//     works: [
+//       {
+//         piece: "Ordo Virtutum",
+//         date: "around 1151",
+//         description: "'play of the virtues': a morality play",
+//       },
+//       {
+//         piece: "O Viridissima Virga",
+//         date: "unknown",
+//         description: "vocal composition, text also by Hildegard",
+//       },
+//       {
+//         piece: "De Sancta Maria",
+//         date: "unknown",
+//         description: "responsorio",
+//       },
+//     ],
+//   },
+//   {
+//     name: "Valerie Coleman",
+//     life: {
+//       fullName: "Valerie Coleman",
+//       lifespan: "1970-present",
+//       bio: "Valerie Coleman is an American flautist and composer, and an extremely influential contemporary artist. She has written many works including pieces for orchestra and wind quintet, which often focus on modern issues. Valerie has had an impressive career so far, including being named Performance Today’s 2020 Classical Woman of the Year and being nominated for a Grammy. More information about Valerie is available on her website, vcolemanmusic.com. ",
+//       nationality: "American",
+//     },
+
+//     era: {
+//       name: "post-modern",
+//       description:
+//         "The latter half of the 20th century into the 21st century. It draws from a wide range of musical styles from various periods, embracing eclecticism and rejecting the strict adherence to certain modernist principles.",
+//     },
+
+//     img: "https://www.vcolemanmusic.com/uploads/3/7/5/0/37502425/02-vcp_headshot.jpg",
+//     imgCredit: "Matthew Murphy",
+//     works: [
+//       {
+//         piece: "Seven O'Clock Shout",
+//         date: "2020",
+//         description: "For orchestra.",
+//       },
+//       {
+//         piece: "Umoja",
+//         date: "2001",
+//         description: "Piece for wind quintet.",
+//       },
+//       {
+//         piece: "Red Clay and Mississippi Delta",
+//         date: "2009",
+//         description: "wind quintet",
+//       },
+//     ],
+//   },
+// ];
